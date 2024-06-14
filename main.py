223d79973818d61dba49c9ec3848dabc8f3e949a
xfoz <mrpizzaguyytb@gmail.com>
2024-06-13 22:35:06 -0400

    initial

---
+++ main.py
@@ -0,0 +1,41 @@
+import flask
+
+# Create the app using flask as a framework
+app = flask.Flask(__name__)
+
+# Index pgae (first page the user sees)
+@app.route("/", methods=["GET"])
+def index():
+    return flask.render_template("index.html")
+
+# About page
+@app.route("/about-us", methods=["GET"])
+def about():
+    return flask.render_template("about.html")
+
+# TOS page
+@app.route("/termsofservice", methods=["GET"])
+def tos():
+    return flask.render_template("tos.html")
+
+# Licensing page
+@app.route("/licensing", methods=["GET"])
+def license():
+    return flask.render_template("license.html")
+
+# Github source redirect
+@app.route("/source", methods=["GET"])
+def source():
+    return flask.redirect("https://github.com/nexus-x86/fullstack-intro/tree/main")
+
+
+
+# Login section
+@app.route("/login", methods=["GET"])
+def login():
+    return flask.render_template("login.html")
+
+
+# Run the python app!
+if __name__ == "__main__":
+    app.run(debug=True)
